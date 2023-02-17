37d597bacb823f0f40882ab3ae455d1bf7dbe72c
gwenbeller <gwenbeller90@gmail.com>
2023-02-17 13:34:19 +0100

    isort, black

--- GigaMath/blog/admin.py
+++ GigaMath/blog/admin.py
@@ -1,21 +1,29 @@
 from django.contrib import admin
 
+from .models import Author, Comment, Post, Tag
+
 # Register your models here.
 
-from .models import Author, Post, Tag, Comment
+
 
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields={
-        "slug":("title",)
-    }
-    list_filter=("author", "date", "tag",)
-    list_display=("title", "excerpt",)
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = (
+        "author",
+        "date",
+        "tag",
+    )
+    list_display = (
+        "title",
+        "excerpt",
+    )
+
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display=("user_name","post")
+    list_display = ("user_name", "post")
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
 admin.site.register(Comment, CommentAdmin)
-
--- GigaMath/blog/models.py
+++ GigaMath/blog/models.py
@@ -1,35 +1,42 @@
-from django.db import models
 from tkinter import CASCADE
-from django.urls import reverse
+
 from django.core.validators import MinLengthValidator
+from django.db import models
+from django.urls import reverse
+
 # Create your models here.
 
+
 class Author(models.Model):
-    first_name=models.CharField(max_length=30)
-    last_name=models.CharField(max_length=120)
-    email=models.EmailField()
-    
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=120)
+    email = models.EmailField()
+
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
-    
+
     def __str__(self):
         return self.full_name()
 
+
 class Tag(models.Model):
-    caption=models.CharField(max_length=30)
+    caption = models.CharField(max_length=30)
 
     def __str__(self):
         return f"{self.caption}"
 
+
 class Post(models.Model):
-    title=models.CharField(max_length=100)
-    excerpt=models.CharField(max_length=400)
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=400)
     image = models.ImageField(upload_to="posts", null=True)
-    date=models.DateField(auto_now=True)
-    slug=models.SlugField(unique=True, default="", blank=True,null=False)
-    content=models.TextField(validators=[MinLengthValidator(10)])
-    author=models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name="posts")
-    tag=models.ManyToManyField(Tag)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, default="", blank=True, null=False)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        Author, null=True, on_delete=models.SET_NULL, related_name="posts"
+    )
+    tag = models.ManyToManyField(Tag)
 
     def __str__(self):
         return f"{self.title}"
@@ -37,8 +44,9 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("post-detail-page", args=[self.slug])
 
+
 class Comment(models.Model):
-    user_name=models.CharField(max_length=120)
-    user_email=models.EmailField()
-    text=models.TextField(max_length=400)
-    post=models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user_name = models.CharField(max_length=120)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
